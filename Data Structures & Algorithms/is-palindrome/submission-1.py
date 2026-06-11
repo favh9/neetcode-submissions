cb175e77c8cb219b2f207357cf529835f6eff3ab
Fausto Alejandro Velazquez Hoyos <151143989+favh9@users.noreply.github.com>
2026-06-11 16:23:40 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,108 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """ Translate
+        A palindrome is a string that reads the same forward and backward.
+        Ignores non-aplhanumeric characters and is case-insensitive.
+        """
+
+        """ Requirements / Output, Input, and Constraints
+        Output: True or False
+        Input: str
+        The given string is made up of all printable ASCII characters.
+        Its length is between 1 and 1000 (inclusive)
+        """
+
+        """ Approach / Methods and Observations
+        1st: 
+        Use two pointers that iterate the string, one that starts at the beginning and another at the end.
+        No need for extra space so no additional data structures are used
+
+        When do we know when the string is a Palindrome?
+            when the string is 1 character
+            when the left pointer crosses the right pointer, or viceversa
+        When do we know it's not a palindrome?
+            when the left pointer's character is valid but does not match the right pointer, or viceversa
+        """
+
+        """ Code / Pseudocode
+        1st Approach:
+        
+        left = 0 
+        right = len(s)-1
+
+        WHILE left_index <= right_index
+            
+            WHILE left_index < right_index AND left_char is invalid
+                left_index++
+            
+            WHILE right_index >= left_index AND right_char is invalid 
+                right_index--
+
+            IF left_index > right_index
+                return True
+
+            IF left_char == right_char
+                right_index--
+                left_index++
+            ELSE
+                return False
+        """
+        
+
+        if len(s) == 1:
+            return True
+
+        left_index = 0
+        right_index = len(s)-1
+
+        while left_index <= right_index:
+
+            while left_index < right_index and not self.isAlphaNum(s[left_index]):
+                left_index += 1
+            
+            while right_index >= left_index and not self.isAlphaNum(s[right_index]):
+                right_index -= 1
+            
+            if left_index > right_index:
+                break
+            
+            elif s[left_index].lower() == s[right_index].lower():
+                right_index -= 1
+                left_index += 1
+            else:
+                return False
+
+        return True
+
+    def isAlphaNum(self, s: str) -> bool:
+
+        # ascii code range for numbers: 48 - 57 inclusive (0-9)
+        # ascii code range for letters: 
+            # 65 - 90 inclusive (A-Z)
+            # 97 - 122 inclsuive (a-z)
+        
+        num_min = ord('0')
+        num_max = ord('9')
+        upper_letter_min = ord('A')
+        upper_letter_max = ord('Z')
+        lower_letter_min = ord('a')
+        lower_letter_max = ord('z')
+        
+        ascii_s = ord(s)
+
+        # numbers
+        if ascii_s >= num_min and ascii_s <= num_max:
+            return True
+        # uppercase letters
+        if ascii_s >= upper_letter_min and ascii_s <= upper_letter_max:
+            return True
+        # lowercase letters
+        if ascii_s >= lower_letter_min and ascii_s <= lower_letter_max:
+            return True
+        
+        return False
+
+
+
+
+        
